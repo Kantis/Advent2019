1f50882b1c0be4e1ac636016c75b8883c73e57a5
Emil Kantis <ext.emil.kantis@rovio.com>
2019-12-13 01:07:56 +0100

    Day 4p2

--- src/main/kotlin/Safecracker.kt
+++ src/main/kotlin/Safecracker.kt
@@ -1,6 +1,7 @@
 fun main() {
-    val count = (248345..746315).filter { twoAdjacentAreTheSame(it) }
+    val count = (248345..746315)
         .filter { isNeverDescending(it) }
+        .filter { evenNumberedRepeatingDigits(it) }
         .count()
     println("Number of potential combinations: $count")
 }
@@ -13,4 +14,28 @@ fun isNeverDescending(number: Int): Boolean {
 fun twoAdjacentAreTheSame(number: Int): Boolean {
     if (number < 10) return false
     return (number % 100) / 10 == number % 10 || twoAdjacentAreTheSame(number / 10)
+}
+
+data class Fragment(
+    val digit: Int,
+    val repetitions: Int
+)
+
+fun fragmentNumber(number: Int): Set<Fragment> {
+    if (number <= 0) return emptySet()
+
+    var divisor = 10
+    val currentDigit = number % 10
+    var repetitions = 1
+
+    while ((number / divisor) % 10 == currentDigit) {
+        divisor *= 10
+        repetitions++
+    }
+
+    return listOf(Fragment(currentDigit, repetitions)).union(fragmentNumber(number / divisor))
+}
+
+fun evenNumberedRepeatingDigits(number: Int): Boolean {
+    return fragmentNumber(number).any { it.repetitions == 2}
 }
--- src/test/kotlin/SafecrackerTests.kt
+++ src/test/kotlin/SafecrackerTests.kt
@@ -1,7 +1,8 @@
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SafecrackerTests {
@@ -24,6 +25,27 @@ class SafecrackerTests {
     @Test
     fun `has descending digits`() {
         assertFalse(isNeverDescending(223450))
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        ints = [11, 12344, 1122, 111122]
+    )
+    fun `even number of repeating digits`(value: Int) {
+        assertTrue(evenNumberedRepeatingDigits(value))
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        ints = [1, 111, 1111, 11111, 123444]
+    )
+    fun `odd number of repeating digits`(value: Int) {
+        assertFalse(evenNumberedRepeatingDigits(value))
+    }
+
+    @Test
+    fun fragment() {
+        assertEquals(setOf(Fragment(1, 3)), fragmentNumber(111))
     }
 
 }
